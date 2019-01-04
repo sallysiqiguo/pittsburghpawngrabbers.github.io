77065936d32da5c0313c130ffded139d45587bad
Sally Guo <siqig@andrew.cmu.edu>
2019-01-04 12:03:15 -0500

    Set up App.js container and Home page

--- src/App.js
+++ src/App.js
@@ -13,30 +13,77 @@ class App extends Component {
     return (
       <Router>
         <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/bio">Bio</Link>
-              </li>
-              <li>
-                <Link to="/stream">Stream</Link>
-              </li>
-              <li>
-                <Link to="/blog">Blog</Link>
-              </li>
-              <li>
-                <a href="https://shop.spreadshirt.com/pghpawngrabbers" target="_blank">Shop</a>
-              </li>
-            </ul>
-          </nav>
+          <div className="container">
+            <div className="header">
+              <div className="title-div">
+                <div id="top-logo">
+                  <a href="/"><img src={require('./images/pawngrabbers_main.png')} width="50" height="50" /></a>
+                </div>
+                <div id="title-text">Pittsburgh Pawngrabbers</div>
+              </div>
+              <nav id="nav-bar">
+                <ul>
+                  <li>
+                    <Link to="/bio">Bio</Link>
+                  </li>
+                  <li>
+                    <Link to="/stream">Stream</Link>
+                  </li>
+                  <li>
+                    <Link to="/blog">Blog</Link>
+                  </li>
+                  <li>
+                    <a href="https://shop.spreadshirt.com/pghpawngrabbers" target="_blank">Shop</a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+            <div className="cover-photo-div">
+              <a href="#"> <img className="cover-photo" src={require("./images/TwitchCoverPGH.jpg")} alt="Cover Photo" /> </a>
+            </div>
 
-          <Route path="/" exact component={() => (<Home />)} />
-          <Route path="/bio" component={() => (<Bio />)} />
-          <Route path="/stream" component={() => (<Stream />)} />
-          <Route path="/blog" component={() => (<Blog />)} />
+            <hr></hr>
+          
+            <Route path="/" exact component={() => (<Home />)} />
+            <Route path="/bio" component={() => (<Bio />)} />
+            <Route path="/stream" component={() => (<Stream />)} />
+            <Route path="/blog" component={() => (<Blog />)} />
+          </div>
+          
+          <div className="fixed">
+            <div className="socialmedia">
+
+              <div className="button">
+                <a href="https://www.facebook.com/PGHpawngrabbers/">
+                  <img src={require('./images/facebooklogo.png')} alt="Facebook" width="30" height="30" />
+                </a>
+              </div>
+
+              <div className="button">
+                <a href="https://twitter.com/pghpawngrabbers?lang=en">
+                  <img src={require('./images/twitterlogo.png')} alt="Twitter" width="30" height="30" />
+                </a>
+              </div>
+
+              <div className="button">
+                <a href="https://www.twitch.tv/pghpawngrabbers">
+                  <img src={require('./images/twitchlogo.png')} alt="Twitch" width="30" height="30" />
+                </a>
+              </div>
+
+              <div className="button">
+                <a href="https://www.youtube.com/channel/UC0HxgtAm7xXKNN_zl5_hJLA">
+                  <img src={require('./images/youtubelogo.png')} alt="Youtube" width="30" height="30" />
+                </a>
+              </div>
+
+              <div className="button">
+                <a href="https://www.instagram.com/pghpawngrabbers/">
+                  <img src={require('./images/insta.png')} alt="Instagram" width="30" height="30" />
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </Router>
     );
--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,53 @@
 import React, { Component } from 'react';
+import { TwitterTimelineEmbed, TwitterShareButton, TwitterFollowButton, TwitterHashtagButton, TwitterMentionButton, TwitterTweetEmbed, TwitterMomentShare, TwitterDMButton, TwitterVideoEmbed, TwitterOnAirButton } from 'react-twitter-embed';
+
 import './App.css';
 
 class Home extends Component {
   render() {
     return (
       <div className="home">
-        <h1>Home</h1>
+        <div ame="pad"></div>
+
+        <div className="row">
+          <div className="column">
+            <div>
+              {/* <a className="twitter-timeline" data-lang="en" data-height="1000" data-width="500" href="https://twitter.com/pghpawngrabbers?ref_src=twsrc%5Etfw">Tweets by pghpawngrabbers</a> 
+              <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> */}
+              <TwitterTimelineEmbed sourceType="profile" screenName="pghpawngrabbers" options={{height: 1000, width: 500}} />
+            </div>
+          </div>
+          <div className="column">
+            <div className="news">
+              <h2>News</h2>
+              <div className="pad"></div>
+              <p>   Welcome to Pittsburgh's Online Pro-Chess Team The Pittsburgh Pawngrabbers new website! This is your one-stop shop for everything Pawngrabber related.</p>
+              <div className="pad"></div>
+              <p>   This will be a feature of our most recent news updates/blog posts on the Blog page.</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="pad"></div>
+
+        <div className="clips"> <h2>Clips</h2> </div>
+
+        <div className="pad"></div>
+
+        <div className="row"> 
+          <div className="column">
+            <iframe src="https://player.twitch.tv/?autoplay=false&video=v325260286" frameborder="0" allowfullscreen="true" scrolling="no" height="300" width="500"></iframe><a className="twitch-desc" href="https://www.twitch.tv/videos/325260286?tt_content=text_link&tt_medium=vod_embed" /><span>Watch Highlight: Pawngrabbers.TV: Nathan makes an easy win much more exciting! from pghpawngrabbers on www.twitch.tv</span>
+          </div>
+          <div className="column">
+            <iframe src="https://player.twitch.tv/?autoplay=false&video=v325260287" frameborder="0" allowfullscreen="true" scrolling="no" height="300" width="500"></iframe><a className="twitch-desc" href="https://www.twitch.tv/videos/325260287?tt_content=text_link&tt_medium=vod_embed" /><span>Watch Highlight: Pawngrabbers.TV: Nathan Forgets his Grob theory! from pghpawngrabbers on www.twitch.tv</span>
+          </div>
+        </div>
+
+        <div className="stats">
+          <div className="pad"></div>
+          <h2>Stats</h2>
+          <div className="pad"></div>
+        </div>
       </div>
     );
   }
